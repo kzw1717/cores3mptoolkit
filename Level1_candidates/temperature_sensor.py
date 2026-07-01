387ee1d58d1a21c975844f6a0608b83caa1baaa9
kzw1717 <watanabekoichi@gmail.com>
2026-07-02 05:51:53 +0900

    ledおよびtemperatureをpin8にした

--- Level1_candidates/temperature_sensor.py
+++ Level1_candidates/temperature_sensor.py
@@ -6,12 +6,13 @@ Grove - Temperature Sensor (SKU: 101020732)
    temperature = 1/(log(R/R0)/B + 1/298.15) - 273.15
 
 対象   : M5Stack CoreS3 + UIFlow2 ファームウェア (MicroPython)
-接続   : PORT.B  ( 黒=GND / 赤=5V / 黄=G9 / 白=G8 )  信号は 黄線 = G9
+接続   : PORT.B  ( 黒=GND / 赤=5V / 黄=G9 / 白=G8 )  ※G8で再テスト中
 実行   : python -m mpremote run temperature_sensor.py
 終了   : Ctrl-C (PC側ターミナルで送信)
 
 注意   : B 定数・基準抵抗はキット付属Arduino資料の値（B=3975 / R0=10kΩ）。
          実機の表示がずれる場合は下の B / R0 を実測で校正してください。
+         ※以前G9では飽和/フローティングで不安定だった。信号がG8側の可能性があり G8 で再テスト。
 """
 
 from machine import Pin, ADC  # type: ignore
@@ -19,7 +20,7 @@ import math  # type: ignore
 import time  # type: ignore
 
 # --- 設定 ---------------------------------------------------------
-SIG_PIN = 9          # PORT.B 黄線 (G9)
+SIG_PIN = 8          # PORT.B 信号ピン（G8で再テスト）
 INTERVAL = 1.0       # loop() の実行間隔 [秒]
 B = 3975             # サーミスタ B 定数
 R0 = 10000           # 基準抵抗 [Ω]
